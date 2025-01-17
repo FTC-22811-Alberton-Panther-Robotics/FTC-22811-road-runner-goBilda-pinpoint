e85569bcd3b21243f7450b3a647e6bd1417cb411
Nathan Morris <nmorris@alberton.k12.mt.us>
2025-01-17 07:09:02 -0700

    Disabled all non-competition code. Moved RRPinpointAuto2 to Competition package and renamed it to ConfigurableAutonomousRR.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/in_development/CompetitionTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/in_development/CompetitionTeleop.java
@@ -29,6 +29,7 @@ package org.firstinspires.ftc.teamcode.in_development;
  */
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -80,7 +81,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
  */
 
 @TeleOp(name="CompetitionTeleop", group="Teleop")
-//@Disabled
+@Disabled
 public class CompetitionTeleop extends LinearOpMode {
 
     private static final double
